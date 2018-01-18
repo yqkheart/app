259e69bd82fd6084c953c91a1ef9807ef53b7089
ice1314 <571706125@qq.com>
2018-01-18 15:02:01 +0800

    'app'

---
+++ comapny/src/js/lzwjs/shanping.js
@@ -0,0 +1,8 @@
+window.onload=function (ev) {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+    });
+};
